0c85bfc577c307c8006ba7a073792471c944f5d9
Celio Cole <celioocole@gmail.com>
2026-05-04 23:20:31 +0200

    Implement Hero Scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,20 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
 
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
-        heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
         gsap.from(heroSplit.chars, {
             yPercent: 100,
@@ -35,50 +41,80 @@ const Hero = () => {
             },
         })
 
-	.to(".right-leaf", { y: 200 }, 0)
-	.to(".left-leaf", { y: -200 }, 0);
+        .to(".right-leaf", { y: 200 }, 0)
+        .to(".left-leaf", { y: -200 }, 0)
 
+      const startValue = isMobile ?  'top 50%' : 'center 60%';
+      const endValue = isMobile ?  '120% top' : 'bottom top';
+
+      const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+      });
+      
+      videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+      };
     }, []);
 
   return (
-    <section id="hero" className="noisy">
-      <div>
-        <h1 className="title">Welcome to El Volador</h1>
-        <p className="subtitle">
-          Experience the thrill of flight with our amazing services.
-        </p>
-        <img 
-            src="/images/hero-left-leaf.png"
-            alt="left-leaf"
-            className="left-leaf"
-        />
-        <img 
-            src="/images/hero-right-leaf.png"
-            alt="right-leaf"
-            className="right-leaf"
-        />
-      </div>
-      <div className="body">
-        <div className="content">
-            <div className="space-y-5 hidden md:block">
-                <p>Cool. Crisp. Classic.</p>
-                <p className="subtitle">
-                   Sip the spirit <br /> of Summer
-                </p>
-            </div>
-            <div className="view-cocktails">
-                <p className="subtitle">
-                    Every cocktail on our menu is a blend of premium ingredients,
-                    creative flair, and timeless recipes — designed to delight your
-                    senses.    
-                </p>
-                <a href="#cocktails">
-                    View Cocktails
-                </a>
-            </div>
+    <>
+      <section id="hero" className="noisy">
+        <div>
+          <h1 className="title">Welcome to<br />El Volador</h1>
+          <p className="subtitle">
+            Experience the thrill of flight with our amazing services.
+          </p>
+          <img 
+              src="/images/hero-left-leaf.png"
+              alt="left-leaf"
+              className="left-leaf"
+          />
+          <img 
+              src="/images/hero-right-leaf.png"
+              alt="right-leaf"
+              className="right-leaf"
+          />
         </div>
-      </div>  
-    </section>
+        <div className="body">
+          <div className="content">
+              <div className="space-y-5 hidden md:block">
+                  <p>Cool. Crisp. Classic.</p>
+                  <p className="subtitle">
+                     Sip the spirit <br /> of Summer
+                  </p>
+              </div>
+              <div className="view-cocktails">
+                  <p className="subtitle">
+                      Every cocktail on our menu is a blend of premium ingredients,
+                      creative flair, and timeless recipes — designed to delight your
+                      senses.    
+                  </p>
+                  <a href="#cocktails">
+                      View Cocktails
+                  </a>
+              </div>
+          </div>
+        </div>  
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />  
+      </div>
+    </>
   );
 };
 
